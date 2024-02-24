36257e3e0c446ddf0215102f4796daec9137c947
Mamun Al Imran <sciemontist@gmail.com>
2024-02-24 16:00:33 +0600

    Edited a database entry and introduced multiple web pages including a master template

--- members/views.py
+++ members/views.py
@@ -1,9 +1,22 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 # Create your views here.
 def members(request):
     #return HttpResponse("Hello World")
-    template = loader.get_template('awal.html')
-    return HttpResponse(template.render())
+    the_members = Member.objects.all().values() # the QuerySet
+    template = loader.get_template('all_members.html')
+    context = {
+    	'the_members': the_members
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+	the_member = Member.objects.get(id = id)
+	template = loader.get_template("details.html")
+	context = {
+		"the_member": the_member
+	}
+	return HttpResponse(template.render(context, request))
